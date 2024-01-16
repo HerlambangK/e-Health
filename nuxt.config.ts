89e8223df59d19057b7850a8a98de2c2a6d07da9
Herlambang Kuswicaksonojati <herlambangk@Herlambangs-MacBook-Air.local>
2024-01-16 22:08:42 +0700

    🛻 Deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,53 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  runtimeConfig: {
-    auth: {
-      secret: process.env.AUTH_SECRET,
-      origin: process.env.AUTH_ORIGIN,
-    },
-    // mongodbUri: process.env.MONGODB_URI,
-  },
-  nitro: {
-    preset: "vercel",
-  },
-
-  build: {
-    transpile: [/echarts/],
-    // optimizeDeps: {
-    //   include: [
-    //     "naive-ui",
-    //     "vueuc",
-    //     "@css-render/vue3-ssr",
-    //     "@juggle/resize-observer",
-    //   ],
-    // },
-  },
-
   modules: [
     "@nuxt/ui",
     "nuxt-server-utils",
-    "@nuxtjs/tailwindcss",
     "@sidebase/nuxt-auth",
     "@nuxt/image",
-    // "nuxt-pdf",
-    // "@bg-dev/nuxt-naiveui",
-    // "@huntersofbook/naive-ui-nuxt",
   ],
 
-  ui: {},
-  nuxtServerUtils: {
-    mongodbUri: process.env.MONGODB_URI,
+  runtimeConfig: {
+    auth: {
+      origin: process.env.AUTH_ORIGIN,
+      secret: process.env.AUTH_SECRET,
+    },
+    // stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    // stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+    // public: {
+    //   pixabayApiKey: process.env.PIXABAY_API_KEY,
+    //   priceId: process.env.STRIPE_PRICE_ID,
+    //   stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
+    // },
   },
+
   auth: {
     baseURL: process.env.AUTH_ORIGIN,
     provider: {
       type: "authjs",
     },
   },
-  tailwindcss: {
-    exposeConfig: true,
-    cssPath: "~/assets/css/input.css",
-    configPath: "tailwind.config",
+  colorMode: {
+    preference: "light",
+  },
+  ui: {},
+  nuxtServerUtils: {
+    mongodbUri: process.env.NEXTAUTH_URL,
   },
 });
