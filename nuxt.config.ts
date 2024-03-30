cfe114bc98dcc62f89e4a13591136bcba7cb98c3
Herlambang Kuswicaksonojati <herlambangk@Herlambangs-MacBook-Air.local>
2024-03-31 04:53:00 +0700

    deploy nuxtimage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/ui",
-    "nuxt-server-utils",
-    "@sidebase/nuxt-auth",
-    "@nuxt/image",
-  ],
-  image: {
-    provider: "netlify",
-    domains: ["ehospitalku.netlify.app"],
-  },
+
   nitro: {
     prerender: {
       routes: [
-        "/_ipx/w_120/market.jpg",
-        "/_ipx/w_140/market.jpg",
+        // "/_ipx/w_120/logo.png",
+        // "/_ipx/w_140/logo.png",
         "/_ipx/_/img/logo.png",
         "/_ipx/_/img/drew-hays-tGYrlchfObE-unsplash.jpg",
         // etc.
       ],
     },
   },
+  modules: [
+    "@nuxt/ui",
+    "nuxt-server-utils",
+    "@sidebase/nuxt-auth",
+    "@nuxt/image",
+  ],
+  // image: {
+  //   provider: "netlify",
+  //   domains: ["ehospitalku.netlify.app"],
+  // },
 
   runtimeConfig: {
     auth: {
