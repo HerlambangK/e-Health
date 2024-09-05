3ccbc09380d066bcc30d5144309555f005b4e6dd
Herlambang Kuswicaksonojati <herlambangk@Herlambangs-MacBook-Air.local>
2024-09-06 00:59:07 +0700

    add trial modal

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -13,6 +13,8 @@ const validationSchema = SigninSchema;
 
 const { signIn } = useAuth();
 const router = useRouter();
+const isOpen = ref(false)
+const countDown = ref(10)
 
 async function handleSubmit(
   event: FormSubmitEvent<z.output<typeof validationSchema>>
@@ -56,10 +58,22 @@ async function handleSubmit(
   }
 }
 
-// import { useSignin } from "~/composables/useSignin";
-useHead({
-  title: "Signin",
-});
+onMounted(() => {
+  isOpen.value = true
+
+  const interval = setInterval(() => {
+    countDown.value--;
+    if (countDown.value <= 0) {
+      clearInterval(interval);
+      isOpen.value = false
+    }
+  }, 1000)
+}),
+
+  // import { useSignin } from "~/composables/useSignin";
+  useHead({
+    title: "Signin",
+  });
 definePageMeta({
   layout: "home",
 });
@@ -67,6 +81,49 @@ definePageMeta({
 // const { formState, isLoading, validationSchema, handleSubmit } = useSignin();
 </script>
 <template>
+  <UModal v-model="isOpen" prevent-close>
+    <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+      <template #header>
+        <div class="flex items-center justify-between">
+          <div class="flex items-center space-x-4">
+            <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+              Trial Account
+            </h3>
+          </div>
+          <div class="font-bold text-red-600">
+            {{ `Close in ${countDown} seconds !` }}
+          </div>
+
+          <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+            @click="isOpen = false" />
+        </div>
+      </template>
+      <div class=" pb-3">
+        <div class="text-md">
+          Silahkan bila ingin mencoba login dengan akun yang tersedia:
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 ">
+          <div class="font-bold">Username: <span
+              class="text-primary-500 underline italic text-sm">deploy@gmail.com</span>
+          </div>
+          <div class="font-bold">Password: <span
+              class="text-primary-500 underline italic text-sm">deploy@gmail.com</span>
+          </div>
+        </div>
+
+        <div class="text-md mt-6">
+          Atau anda dapat Melakukan Regristrasi:
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 ">
+        </div>
+        <UButton to="/auth/signup" size="2xs" variant="solid">Registrasi</UButton>
+
+
+      </div>
+
+      <Placeholder class="h-32" />
+    </UCard>
+  </UModal>
   <WrapperAuth title="Sign In to your account">
     <template #header>
       <span class="text-sm mr-px">Don't have an account?</span>
@@ -75,18 +132,10 @@ definePageMeta({
 
     <UForm :state="formState" :schema="validationSchema" @submit="handleSubmit">
       <UFormGroup class="mb-4" name="email" label="Email">
-        <UInput
-          v-model="formState.email"
-          type="email"
-          placeholder="john@email.com"
-        />
+        <UInput v-model="formState.email" type="email" placeholder="john@email.com" />
       </UFormGroup>
       <UFormGroup class="mb-4" name="password" label="Password">
-        <UInput
-          v-model="formState.password"
-          type="password"
-          placeholder="********"
-        />
+        <UInput v-model="formState.password" type="password" placeholder="********" />
       </UFormGroup>
       <UFormGroup>
         <UButton :loading="isLoading" type="submit" color="primary" block>
@@ -95,6 +144,9 @@ definePageMeta({
       </UFormGroup>
     </UForm>
   </WrapperAuth>
+
+
+
 </template>
 
 <style>
@@ -108,9 +160,11 @@ definePageMeta({
   0% {
     background-position: 0% 50%;
   }
+
   50% {
     background-position: 100% 50%;
   }
+
   100% {
     background-position: 0% 50%;
   }
